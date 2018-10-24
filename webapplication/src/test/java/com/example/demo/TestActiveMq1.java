47e6925d38981b74c31a9f9ea88453958a7a8320
goto_tony@163.com <goto_tony@163.com>
2018-10-24 16:56:05 +0800

    commit

--- webapplication/src/test/java/com/example/demo/TestActiveMq1.java
+++ webapplication/src/test/java/com/example/demo/TestActiveMq1.java
@@ -3,13 +3,14 @@ package com.example.demo;
 
 import javax.jms.Destination;
 
-import com.example.activemq.QueueProducer;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.activemq.QueueProducer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
